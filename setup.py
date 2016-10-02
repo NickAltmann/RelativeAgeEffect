e2acfeb1e466e069c32e64818b910e298bd69b42
NickAltmann <nick@nickaltmann.net>
2016-10-02 13:59:27 +0100

    Modify setup

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
-setup(name='relative_age',
+setup(name='relativeage',
       version='0.1',
       description='Relative age effect investigation.',
       url='https://github.com/NickAltmann/RelativeAgeEffect',
       author='Nick Altmann',
       author_email='nick@nickaltmann.net',
       license='MIT',
-      packages=['relative_age'],
+      packages=['relativeage'],
       install_requires=[
           'scipy', 'beautifulsoup4', 'pandas', 'requests', 'lxml'
       ],
-      zip_safe=False)
+      zip_safe=True)
